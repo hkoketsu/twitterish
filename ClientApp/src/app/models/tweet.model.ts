16677029f24759b82d360ad8e87dd923eab75e62
Hiroki Koketsu <hiroki.koketsu@gmail.com>
2020-04-13 08:08:26 -0700

    Add deselialier to tweet model

--- ClientApp/src/app/models/tweet.model.ts
+++ ClientApp/src/app/models/tweet.model.ts
@@ -1,5 +1,10 @@
-export class Tweet {
+import { Deserializable } from './deserializable.model';
+
+export class Tweet implements Deserializable {
   id: number;
   body: string;
 
+  deserialize(input: any): this {
+    return Object.assign(this, input);
+  }
 }
