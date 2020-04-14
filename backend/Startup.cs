50c821b1b91d33eb708c66582cda66eeea0c021b
Hiroki Koketsu <hiroki.koketsu@gmail.com>
2020-04-14 07:16:11 -0700

    Remove spa and razor from backend

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 // using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,13 +37,6 @@ namespace Twitterish
                 .AddIdentityServerJwt();
             
             services.AddControllersWithViews();
-            // services.AddRazorPages();
-
-            // In production, the Angular files will be served from this directory
-            // services.AddSpaStaticFiles(configuration =>
-            // {
-            //     configuration.RootPath = "ClientApp/dist";
-            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,13 +53,6 @@ namespace Twitterish
                 app.UseHsts();
             }
 
-            // app.UseHttpsRedirection();
-            // app.UseStaticFiles();
-            // if (!env.IsDevelopment())
-            // {
-            //     app.UseSpaStaticFiles();
-            // }
-
             app.UseRouting();
 
             app.UseAuthentication();
@@ -79,21 +64,7 @@ namespace Twitterish
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
-                // endpoints.MapRazorPages();
             });
-
-            // app.UseSpa(spa =>
-            // {
-            //     // To learn more about options for serving an Angular SPA from ASP.NET Core,
-            //     // see https://go.microsoft.com/fwlink/?linkid=864501
-
-            //     spa.Options.SourcePath = "ClientApp";
-
-            //     if (env.IsDevelopment())
-            //     {
-            //         spa.UseAngularCliServer(npmScript: "start");
-            //     }
-            // });
         }
     }
 }
